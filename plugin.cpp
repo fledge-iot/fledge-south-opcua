a29a6a677a485aedc86056e37b685426fb9da3f9
Vaibhav Singhal <vaibhav@scaledb.com>
2018-12-07 15:32:41 +0530

    1.4.5 Release Candidate

--- plugin.cpp
+++ plugin.cpp
@@ -45,7 +45,7 @@ extern "C" {
  */
 static PLUGIN_INFORMATION info = {
 	"upcua",                  // Name
-	"1.0.0",                  // Version
+	"1.4.5",                  // Version
 	SP_ASYNC, 		  // Flags
 	PLUGIN_TYPE_SOUTH,        // Type
 	"1.0.0",                  // Interface version
