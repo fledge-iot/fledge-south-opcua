743d43b417700dd745af56207587d3971d57bb44
Ashwini Kumar Pandey <ashwini165@gmail.com>
2024-12-23 20:04:13 +0530

    Fixed the plugin crash issue during disable
    Signed-off-by: Ashwini Kumar Pandey <ashwini165@gmail.com>

--- opcua.cpp
+++ opcua.cpp
@@ -722,18 +722,28 @@ int n_subscriptions = 0;
 /**
  * Stop all subscriptions and disconnect from the OPCUA server
  */
-void
-OPCUA::stop()
+void OPCUA::stop()
 {
-	if (m_connected)
+	try
 	{
-		subscriptionVariables.clear();
-		m_assetPathNames.clear();
-		m_client->Disconnect();
+		if (m_connected)
+		{
+			subscriptionVariables.clear();
+			m_assetPathNames.clear();
+			m_client->Disconnect();			
+		}
+		if (m_client)
+		{
+			delete m_client;
+		}
 	}
-	if (m_client)
+	catch (const std::exception& e) 
 	{
-		delete m_client;
+		Logger::getLogger()->error("Error during stop: %s", e.what());
+	} 
+	catch (...) 
+	{
+		Logger::getLogger()->error("An unknown error occurred during stop.");
 	}
 }
 
